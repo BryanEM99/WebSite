17a26c570a10b31cc4a652cda273d3af6cc816fa
BryanEM99 <bryanesp45@gmail.com>
2024-04-02 09:38:35 -0500

    php

--- enviar_formulario.php
+++ enviar_formulario.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // Verificar si se han enviado datos del formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -33,39 +32,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header("Location: index.html");
 }
 ?>
-=======
-<?php
-// Verificar si se han enviado datos del formulario
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recoger los datos del formulario
-    $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
-    $telefono = $_POST['telefono'];
-    $asunto = $_POST['asunto'];
-    $mensaje = $_POST['mensaje'];
-
-    // Dirección de correo electrónico a la que se enviará el mensaje
-    $destinatario = "tucorreo@example.com";
-
-    // Asunto del correo electrónico
-    $asunto_email = "Nuevo mensaje de formulario de contacto";
-
-    // Construir el cuerpo del mensaje
-    $cuerpo_email = "Nombre: $nombre\n";
-    $cuerpo_email .= "Email: $email\n";
-    $cuerpo_email .= "Teléfono: $telefono\n";
-    $cuerpo_email .= "Asunto: $asunto\n";
-    $cuerpo_email .= "Mensaje: $mensaje\n";
-
-    // Enviar el correo electrónico
-    if (mail($destinatario, $asunto_email, $cuerpo_email)) {
-        echo "¡Gracias por contactarnos! Su mensaje ha sido enviado correctamente.";
-    } else {
-        echo "Lo siento, hubo un error al enviar su mensaje. Por favor, inténtelo de nuevo más tarde.";
-    }
-} else {
-    // Si se accede al script directamente sin enviar datos del formulario, redirigir al formulario de contacto
-    header("Location: index.html");
-}
-?>
->>>>>>> 8971ed22939199c036fbb128e5bd310380742c83
